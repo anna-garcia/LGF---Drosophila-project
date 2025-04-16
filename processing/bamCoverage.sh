fe487fc00feb140a88374f1f00497c046dbcad1a
Anna Carolina Garcia <70608263+anna-garcia@users.noreply.github.com>
2025-04-16 15:11:09 -0300

    Update bamCoverage.sh

--- processing/bamCoverage.sh
+++ processing/bamCoverage.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
-set -euo pipefail
+# listing mapping files folder:
 
-# inputs
-bamfile=$1
-sf_file="/data/analysis/carol/drosophila/Drosophila_snakemake/parental/results/mapping/scalefactor.tsv"
-outdir="/data/analysis/carol/drosophila/Drosophila_snakemake/parental/results/mapping/bam_cov_anna"
+mapping_files=`ls ../*.bam`
 
-# outputs
-basename=$(basename $bamfile)
-prefix=${basename%.*}
+for bamfile in $mapping_files
+do
+    # inputs:
+    basename=$(basename $bamfile)
+    prefix=${basename%.*}
 
-sense="$outdir/${prefix}.sense.bw"
-antisense="$outdir/${prefix}.antisense.bw"
+    sf_file="/data/analysis/carol/drosophila/Drosophila_snakemake/parental/results/mapping/scalefactor.tsv"
+    outdir="/data/analysis/carol/drosophila/Drosophila_snakemake/parental/results/mapping/bam_cov_anna"
 
-## Get Scale Factor
-sample=""
+    # outputs:
 
-sample=$(echo $basename | awk '{split($0,x,"."); print x[1]}')
-sf=$(grep $sample $sf_file | cut -f 2)
+    sense="$outdir/${prefix}.sense.bw"
+    antisense="$outdir/${prefix}.antisense.bw"
 
-echo "$basename , $sample ,  $sf"
+    ## Getiing Scale Factor:
 
-## Generating sense coverage:
+    sample=""
 
-params="--samFlagInclude 0 -bs 10 -p 8 --scaleFactor $sf"
-bamCoverage -b $bamfile $params -o $sense 
+    sample=$(echo $basename | awk '{split($0,x,"."); print x[1]}')
+    sf=$(grep $sample $sf_file | cut -f 2)
 
-## Generating antisense coverage:
+    echo "$basename , $prefix , $sample ,  $sf"
+    ## Generating sense coverage:
 
-params="--samFlagInclude 16 -bs 10 -p 8 --scaleFactor $sf"
-bamCoverage -b $bamfile $params -o $antisense 
+    params="--samFlagInclude 0 -bs 10 -p 8 --scaleFactor $sf"
+    bamCoverage -b $bamfile $params -o $sense
+
+    ## Generating antisense coverage:
+
+    params="--samFlagInclude 16 -bs 10 -p 8 --scaleFactor $sf"
+    bamCoverage -b $bamfile $params -o $antisense
+
+    echo "DONE $bamfile !!!"
+done
+
+echo "DONE FULL FOLDER"
