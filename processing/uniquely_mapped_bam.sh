763fd65e2cc5598beec23cd265245aa7c7fee05f
Anna Carolina Garcia <70608263+anna-garcia@users.noreply.github.com>
2025-04-16 16:03:34 -0300

    Update uniquely_mapped_bam.sh

--- processing/uniquely_mapped_bam.sh
+++ processing/uniquely_mapped_bam.sh
@@ -5,7 +5,7 @@ mapping_files=`ls ./*.bam`
 for bamfile in $mapping_files
 do
     basename=$(basename $bamfile)
-    output={$basename}.unique.bam
+    output=${basename}.unique.bam
     awk '{{if($0~"@"){print $0}else{if($2!="4" && $15=="XM:i:1"){print $0}}}}' <(samtools view -h $bamfile) |
     samtools view -Sb &&
     samtools index $output
