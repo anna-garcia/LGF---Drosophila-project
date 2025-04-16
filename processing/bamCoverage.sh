735063fa1c07ddd78db97822a1e84e5e7efa2671
Anna Carolina Garcia <70608263+anna-garcia@users.noreply.github.com>
2025-04-16 12:35:48 -0300

    Update bamCoverage.sh

--- processing/bamCoverage.sh
+++ processing/bamCoverage.sh
@@ -16,14 +16,12 @@ antisense="$outdir/${prefix}.antisense.bw"
 
 ## Get Scale Factor
 sample=""
-if [[ $basename =~ vs ]]; then
-    sample=$(echo $basename | awk '{split($0,x,"."); print x[1]}')
-    sf=$(grep $sample $sf_file | cut -f 2)
-else
-    sf=$(grep $prefix $sf_file | cut -f 2)
-fi
 
-echo "$basename , $sf"
+sample=$(echo $basename | awk '{split($0,x,"."); print x[1]}')
+sf=$(grep $sample $sf_file | cut -f 2)
+
+echo "$basename , $sample ,  $sf"
+
 ## Generating sense coverage:
 
 params="--samFlagInclude 0 -bs 10 -p 8 --scaleFactor $sf"
